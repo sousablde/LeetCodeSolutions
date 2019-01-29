b262029c6fa39d99354f673273fd904ccbb75d87
sousablde <sousablde@gmail.com>
2019-01-28 22:56:51 -0800

    working solution

---
+++ SquaresOfSortedArray/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] A = {-4, -1, 0, 3, 10};
+
+        Solution solution = new Solution();
+
+        System.out.println(Arrays.toString(solution.sortedSquares(A)));
+    }
+}
