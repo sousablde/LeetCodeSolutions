7145b7c3ddd2cf7683cb0583c548377571939bb7
sousablde <sousablde@gmail.com>
2019-01-28 20:04:06 -0800

    failed solution

---
+++ AddBinaryNumbers/src/Main.java
@@ -0,0 +1,16 @@
+/*
+exercise: https://leetcode.com/problems/add-binary/submissions/
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String a = "00";
+        String b = "0";
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.addBinary(a, b));
+
+    }
+}
