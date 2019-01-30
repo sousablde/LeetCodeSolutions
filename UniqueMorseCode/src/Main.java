d32331a99254b4e8f4afa98c4ac76cfb205f015e
sousablde <sousablde@gmail.com>
2019-01-30 15:02:58 -0800

    2 solutions for hashmaps

---
+++ UniqueMorseCode/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] words = {"gin", "zen", "gig", "msg"};
+
+        Solution solution = new Solution();
+
+        System.out.println(solution.uniqueMorseRepresentations(words));
+    }
+}
