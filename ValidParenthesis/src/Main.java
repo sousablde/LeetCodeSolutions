c3991f2f7a0967f1b3f15d8099ec2794b33bd8a7
sousablde <sousablde@gmail.com>
2019-01-31 08:50:16 -0800

    working solution

---
+++ ValidParenthesis/src/Main.java
@@ -0,0 +1,14 @@
+/*
+https://leetcode.com/problems/valid-parentheses/
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s = "()[]{}";
+        Solution solution = new Solution();
+
+        System.out.println(solution.isValid(s));
+    }
+}
