085e8aa83db9abfa1d916d33d0e3591f01d6ae69
sousablde <sousablde@gmail.com>
2019-02-01 15:14:31 -0800

    working solution

---
+++ nRepeatedElement2NArray/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] A = {5, 1, 5, 2, 5, 3, 5, 4};
+        Solution solution = new Solution();
+
+        System.out.println(solution.repeatedNTimes(A));
+    }
+
+}
